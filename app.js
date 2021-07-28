789504f7c9f714c793ef8beed7890d50a684e9fc
leebj <redgpcmffld@gmail.com>
2021-07-28 13:39:34 +0900

    port change

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ io.on('connection', socket => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Socket IO listening on port 3000');
+server.listen(8000, () => {
+    console.log('Socket IO listening on port 8000');
 });
